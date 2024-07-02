e3d7ed998bd54dbe0b151ae62a7a0a2fa635c0dc
Roman_Tretiakov <romantre@wizardsdev.com>
2024-07-03 00:36:50 +0300

    Add HW1

---
+++ module_3_1_hw.py
@@ -0,0 +1,31 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+
+    for str_ in list_to_search:
+        a = str_.lower()
+        b = string.lower()
+        c = a.find(b)
+        if str_.lower().find(string.lower()) >= 0:
+            return True
+
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
